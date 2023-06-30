3dc60ca6f7b5d8e8f864189bc1cf7ef60fff06e1
Alexis Manin <alexis.manin@gmail.com>
2023-06-30 16:09:04 +0200

    Spring demo: autowire configuration parameter through kotlin context receiver

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-kt-ctx-receiver-demo"
---
+++ src/main/kotlin/KtCtxReceiverDemoApplication.kt
@@ -0,0 +1,38 @@
+package fr.amanin.demo.spring.ktctxreceiverdemo
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+
+@SpringBootApplication
+class KtCtxReceiverDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtCtxReceiverDemoApplication>(*args)
+}
+
+@Component
+class AppInfo(@Value("\${spring.application.name}") val name: String)
+
+context(AppInfo)
+class ContextBased(val source: String) {
+	init {
+		println(
+			"""
+            >>
+            >> Injected from $source: $name
+            >>
+            """.trimIndent()
+		)
+	}
+}
+
+@Configuration
+class ByConfiguration {
+	context(AppInfo)
+	@Bean("from-configuration")
+	fun create(): ContextBased = ContextBased("configuration")
+}
